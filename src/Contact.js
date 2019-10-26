95137833c67dbf3c584f3e8c3768795e96f07332
EC2 Default User <ec2-user@ip-172-31-86-4.ec2.internal>
2019-10-26 23:05:39 +0000

    layout of website

---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+  
+  constructor(props){
+      super(props);
+      this.state = {}
+  }
+  
+  
+  render(){
+  return (
+    <div>
+        Contact
+    </div>
+  );
+  }
+}
+
+export default Contact;
---
+++ src/FeatureWall.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class FeatureWall extends Component {
+  
+  constructor(props){
+      super(props);
+      this.state = {}
+  }
+  
+  
+  render(){
+  return (
+    <div>
+        FeatureWall
+    </div>
+  );
+  }
+}
+
+export default FeatureWall;
