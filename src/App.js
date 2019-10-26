ebbfe83d7d042e23baf0e3857f1527f0b34c1915
EC2 Default User <ec2-user@ip-172-31-33-230.ec2.internal>
2019-10-26 20:05:04 +0000

    Second Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calender from './Calender.js';
+
+function App() {
+  return (
+    <div>
+        <Calender/>
+        
+    </div>
+  );
+}
+
+export default App;
